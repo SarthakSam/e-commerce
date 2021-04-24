92a723d4c38b87aa15cc07c70468083ab108ba1e
Sarthak <sarthak242@gmail.com>
2021-04-24 20:47:21 +0530

    Feat: added signup and signin functionality

---
+++ backend/apis/user.api.js
@@ -0,0 +1,45 @@
+const express = require('express'),
+      router  = express.Router(),
+      User    = require('../models/user.model');
+
+router.post('/signup', async (req, res) => {
+    const { username, password1, password2 } = req.body;
+    if( !username || !password1 || !password2 ) {
+        return res.status(500).json({ error: 'Please enter all the required fields' });
+    }
+    if( password1 !== password2 ) {
+        return res.status(500).json({ error: 'Passwords doesnt match' });
+    }
+    // if ( ( await User.find( { usernamme } ) ) ) {
+    //     return res.status(500).json({ error: 'User with this username already exists' });
+    // }
+    try {
+        const user = User.create({ username, password: password1 })
+        if(!user) {
+            return res.status(500).json({ error: 'Error while creating new user' });
+        }
+        res.status(201).json({ message: 'success', user: { username: user.username, _id: user._id } });
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: err});
+    }
+});
+
+router.post('/signin', async (req, res) => {
+    const { username, password } = req.body;
+    if( !username || !password ) {
+        return res.status(500).json({ error: 'Please enter all the required fields' });
+    }
+    try {
+        const user = User.find({ username });
+        if(!user) {
+            res.status(500).json({ error: 'No such user exists' });
+        }
+        if(user.password !== password) 
+            return res.send(500).json({ error: 'Password incorrect' });
+        res.status(200).json({ message: 'success', user: { username: user.username, _id: user._id } });
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: err});
+    }
+});
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,9 +2,9 @@ const mongoose          = require('mongoose'),
       { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-    username: { type: String, required: 'Username is mandatory' },
+    username: { type: String, required: 'Username is mandatory', unique: 'This username already exists. Please choose a unique username' },
     password: { type: String, required: 'Password is mandatory' },
-    email: { type: String },
+    email: { type: String, unique: 'This email already exists. Please enter a valid email' },
     wishlist: [ { type: Schema.Types.ObjectId, ref: 'Product' } ],
     cart: [ { type: Schema.Types.ObjectId, ref: 'ProductQuantityMapping' } ]
 } , { timestamps: true })
